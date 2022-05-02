f0e7ab18109cdf9868fe7db1cd748772fcfbddbe
lorrydriveloper <lorrydriveloper@gmail.com>
2022-05-02 15:35:33 +0100

    delete gem build

--- lib/minirails.rb
+++ lib/minirails.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
-require_relative "minirails/version"
+require_relative 'minirails/version'
 
 module Minirails
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Application
+    def call(_env)
+      [
+        200,
+        { 'Content-Type' => 'text/html' },
+        ['Hello, from Ruby on miniRails'],
+      ]
+    end
+  end
 end
